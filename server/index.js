c9d9d5f65b5ce36aace7e5f75ebb1b44296446be
solleedata <solleedata@gmail.com>
2021-10-17 10:56:36 +0900

    feat

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const app = require('express')()
+const app = require('express')
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
@@ -8,6 +8,7 @@ io.on('connection', socket => {
   })
 })
 
-http.listen(4000, function() {
+http.listen(4000, function () {
   console.log('listening on port 4000')
 })
+
